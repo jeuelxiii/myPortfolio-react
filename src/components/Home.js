94cbe7679680ff26c4d470417a688c61948135ae
jeuelxiii <jeuel.joaquin@gmail.com>
2020-12-31 20:16:56 +0800

    adding sanity

---
+++ src/components/Home.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react'
+import '../App.css'
+import {Container} from '@material-ui/core'
+
+class Home extends Component {
+    render() {
+        return (
+            <div className="box">
+                <Container>
+                <h1>Home Page</h1>
+                </Container>
+            </div>
+        )
+    }
+}
+
+export default Home;
